104160a8fcd5030a6e828be1b6d626e11a54b1e4
harryo583 <hongyiwang2027@u.northwestern.edu>
2025-12-27 14:29:31 +0100

    new class defined

--- black_scholes.py
+++ black_scholes.py
@@ -1,6 +1,15 @@
 import math
 from typing import Dict, List, Tuple, Callable, Optional
 
+class Params:
+    S: float # stock price
+    K: float # strike price
+    T: float # time in years
+    r: float # continuously compounded risk-free rate
+    sigma: float # volatility
+    q: float # divident yield
+
 class BlackScholes:
     def __init__(self):
+        
         pass
