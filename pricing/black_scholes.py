425054bc7ac7cf374432a0a1260f0cae2a4762a9
harryo583 <hongyiwang2027@u.northwestern.edu>
2026-02-25 15:46:25 -0600

    greek specs done

--- pricing/black_scholes.py
+++ pricing/black_scholes.py
@@ -1,5 +1,5 @@
 import math
-from typing import Dict, List, Tuple, Callable, Optional
+from typing import Dict, List, Tuple, Callable, Optional, Iterable
 from dataclasses import dataclass, replace
 
 from pricing.diff import DiffConfig, finite_diff, step
@@ -53,13 +53,68 @@ class BlackScholes:
         self._rho = Greek("rho", [("r", 1)])
 
         # Second order greeks
-        self._vanna = Greek()
-        self._volga = Greek()
-        self._charm = Greek()
-        self._vega_decay = Greek() 
+        self._vanna = Greek("vanna", [("S", 1), ("sigma", 1)])
+        self._volga = Greek("volga", [("sigma", 2)])
+        self._charm = Greek("charm", [("S", 1), ("T", 1)], theta_market=True)
+        self._vega_decay = Greek("charm", [("sigma", 1), ("T", 1)], theta_market=True)
 
         # Third order greeks
-        self._speed = None
-        self._color = None
-        self._ultima = None
-        self._zomma = None
+        self._speed = Greek("speed", [("S", 3)])
+        self._color = Greek("color", [("S", 2), ("T", 1)], theta_market=True)
+        self._ultima = Greek("ultima", [("sigma", 3)])
+        self._zomma = Greek("zomma", [("S", 2), ("sigma", 1)])
+
+
+    @property
+    def price(self) -> Greek: return self._price
+
+    @property
+    def delta(self) -> Greek: return self._delta
+    
+    @property
+    def gamma(self) -> Greek: return self._gamma
+
+    @property
+    def vega(self) -> Greek: return self._vega
+
+    @property
+    def theta(self) -> Greek: return self._theta
+
+    @property
+    def rho(self) -> Greek: return self._rho
+
+    @property
+    def vanna(self) -> Greek: return self._vanna
+
+    @property
+    def volga(self) -> Greek: return self._volga
+
+    @property
+    def charm(self) -> Greek: return self._charm
+
+    @property
+    def vega_decay(self) -> Greek: return self._vega_decay
+
+    @property
+    def speed(self) -> Greek: return self._speed
+
+    @property
+    def color(self) -> Greek: return self._color
+
+    @property
+    def ultima(self) -> Greek: return self._ultima
+
+    @property
+    def zomma(self) -> Greek: return self._zomma
+
+
+    def greeks(self) -> Iterable[Greek]:
+        return [
+            self.price, self.delta, self.gamma, self.vega, self.theta, self.rho,
+            self.vanna, self.volga, self.charm, self.vega_decay,
+            self.speed, self.color, self.ultima, self.zomma
+        ]
+    
+
+    def _price_value(self, c: Contract, m: Market) -> float:
+        pass
