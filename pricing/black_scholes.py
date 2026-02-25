31da0080cedf41312661d1f18c99deb4c6cfe378
harryo583 <hongyiwang2027@u.northwestern.edu>
2026-02-25 15:54:12 -0600

    changed to no private attributes - less safe but cleaner

--- pricing/black_scholes.py
+++ pricing/black_scholes.py
@@ -43,69 +43,26 @@ class BlackScholes:
         self.diff = diff 
 
         # Zeroth order
-        self._price = Greek("price", [])
+        self.price = Greek("price", [])
         
         # First order greeks
-        self._delta = Greek("delta", [("S", 1)])
-        self._gamma = Greek("gamma", [("S", 2)])
-        self._vega = Greek("vega", [("sigma", 1)])
-        self._theta = Greek("theta", [("T", 1)], theta_market=True)
-        self._rho = Greek("rho", [("r", 1)])
+        self.delta = Greek("delta", [("S", 1)])
+        self.gamma = Greek("gamma", [("S", 2)])
+        self.vega = Greek("vega", [("sigma", 1)])
+        self.theta = Greek("theta", [("T", 1)], theta_market=True)
+        self.rho = Greek("rho", [("r", 1)])
 
         # Second order greeks
-        self._vanna = Greek("vanna", [("S", 1), ("sigma", 1)])
-        self._volga = Greek("volga", [("sigma", 2)])
-        self._charm = Greek("charm", [("S", 1), ("T", 1)], theta_market=True)
-        self._vega_decay = Greek("charm", [("sigma", 1), ("T", 1)], theta_market=True)
+        self.vanna = Greek("vanna", [("S", 1), ("sigma", 1)])
+        self.volga = Greek("volga", [("sigma", 2)])
+        self.charm = Greek("charm", [("S", 1), ("T", 1)], theta_market=True)
+        self.vega_decay = Greek("charm", [("sigma", 1), ("T", 1)], theta_market=True)
 
         # Third order greeks
-        self._speed = Greek("speed", [("S", 3)])
-        self._color = Greek("color", [("S", 2), ("T", 1)], theta_market=True)
-        self._ultima = Greek("ultima", [("sigma", 3)])
-        self._zomma = Greek("zomma", [("S", 2), ("sigma", 1)])
-
-
-    @property
-    def price(self) -> Greek: return self._price
-
-    @property
-    def delta(self) -> Greek: return self._delta
-    
-    @property
-    def gamma(self) -> Greek: return self._gamma
-
-    @property
-    def vega(self) -> Greek: return self._vega
-
-    @property
-    def theta(self) -> Greek: return self._theta
-
-    @property
-    def rho(self) -> Greek: return self._rho
-
-    @property
-    def vanna(self) -> Greek: return self._vanna
-
-    @property
-    def volga(self) -> Greek: return self._volga
-
-    @property
-    def charm(self) -> Greek: return self._charm
-
-    @property
-    def vega_decay(self) -> Greek: return self._vega_decay
-
-    @property
-    def speed(self) -> Greek: return self._speed
-
-    @property
-    def color(self) -> Greek: return self._color
-
-    @property
-    def ultima(self) -> Greek: return self._ultima
-
-    @property
-    def zomma(self) -> Greek: return self._zomma
+        self.speed = Greek("speed", [("S", 3)])
+        self.color = Greek("color", [("S", 2), ("T", 1)], theta_market=True)
+        self.ultima = Greek("ultima", [("sigma", 3)])
+        self.zomma = Greek("zomma", [("S", 2), ("sigma", 1)])
 
 
     def greeks(self) -> Iterable[Greek]:
